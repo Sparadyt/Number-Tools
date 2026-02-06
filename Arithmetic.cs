a5a416f318d492ce9ad2725320efa31619bfd5d7
Sparadyt <Sparadyt@gmail.com>
2026-02-06 18:33:20 +0545

    Complicated:    *Substraction    *Multiplication    *Division
    Made that you could see both inputs in the result

--- Arithmetic.cs
+++ Arithmetic.cs
@@ -46,23 +46,128 @@ public static class Arithmetic
 
     public static void Subtraction()
     {
-        Console.Clear();
-        int baseInt = Home.GetBase("SUBTRACTION");
-        Console.ReadKey();
+        bool newBase = false;
+        int repeated = 0;
+        int baseInt = 0; //Value gets changed immediately
+
+        while(true)
+        {
+            Console.Clear();
+
+            //Getting base if first time or requested
+            if (repeated == 0 || newBase)
+            {
+                baseInt = Home.GetBase("SUBTRACTION");
+                newBase = false;
+            }
+
+            //Getting both inputs
+            string input1 = Home.GetInput(baseInt, null).ToString();
+            string input2 = Home.GetInput(baseInt, input1).ToString();
+
+            //Printing base and inputs
+            Console.WriteLine($"BASE {baseInt}");
+            Console.WriteLine($"First Number: {input1}");
+            Console.WriteLine($"Second Number: {input2}");
+            ConsoleKeyInfo key = GetAndPrintResult(input1, input2, baseInt, '-');
+
+            //Geting end result
+            if (key.Key == ConsoleKey.Escape || key.KeyChar == 'e' || key.KeyChar == 'E')
+            {
+                break;
+            }
+
+            else if (key.KeyChar == 'b' || key.KeyChar == 'B')
+            {
+                newBase = true;
+            }
+
+            repeated++;
+        }
     }
 
     public static void Multiplication()
     {
-        Console.Clear();
-        int baseInt = Home.GetBase("MULTIPLICATION");
-        Console.ReadKey();
+        bool newBase = false;
+        int repeated = 0;
+        int baseInt = 0; //Value gets changed immediately
+
+        while(true)
+        {
+            Console.Clear();
+
+            //Getting base if first time or requested
+            if (repeated == 0 || newBase)
+            {
+                baseInt = Home.GetBase("MULTIPLICATION");
+                newBase = false;
+            }
+
+            //Getting both inputs
+            string input1 = Home.GetInput(baseInt, null).ToString();
+            string input2 = Home.GetInput(baseInt, input1).ToString();
+
+            //Printing base and inputs
+            Console.WriteLine($"BASE {baseInt}");
+            Console.WriteLine($"First Number: {input1}");
+            Console.WriteLine($"Second Number: {input2}");
+            ConsoleKeyInfo key = GetAndPrintResult(input1, input2, baseInt, '*');
+
+            //Geting end result
+            if (key.Key == ConsoleKey.Escape || key.KeyChar == 'e' || key.KeyChar == 'E')
+            {
+                break;
+            }
+
+            else if (key.KeyChar == 'b' || key.KeyChar == 'B')
+            {
+                newBase = true;
+            }
+
+            repeated++;
+        }
     }
 
     public static void Division()
     {
-        Console.Clear();
-        int baseInt = Home.GetBase("DIVISION");
-        Console.ReadKey();
+        bool newBase = false;
+        int repeated = 0;
+        int baseInt = 0; //Value gets changed immediately
+
+        while(true)
+        {
+            Console.Clear();
+
+            //Getting base if first time or requested
+            if (repeated == 0 || newBase)
+            {
+                baseInt = Home.GetBase("DIVISION");
+                newBase = false;
+            }
+
+            //Getting both inputs
+            string input1 = Home.GetInput(baseInt, null).ToString();
+            string input2 = Home.GetInput(baseInt, input1).ToString();
+
+            //Printing base and inputs
+            Console.WriteLine($"BASE {baseInt}");
+            Console.WriteLine($"First Number: {input1}");
+            Console.WriteLine($"Second Number: {input2}");
+            ConsoleKeyInfo key = GetAndPrintResult(input1, input2, baseInt, '/');
+
+            //Geting end result
+            if (key.Key == ConsoleKey.Escape || key.KeyChar == 'e' || key.KeyChar == 'E')
+            {
+                break;
+            }
+
+            else if (key.KeyChar == 'b' || key.KeyChar == 'B')
+            {
+                newBase = true;
+            }
+
+            repeated++;
+        }
     }
 
     public static ConsoleKeyInfo GetAndPrintResult(string input1, string input2, int baseInt, char operation)
@@ -91,11 +196,17 @@ public static class Arithmetic
         {
             preResult = Convert.ToInt32(input1, baseInt) / Convert.ToInt32(input2, baseInt);
         }
-    
+
         //Printing result
         string result = Convert.ToString(preResult, baseInt);
+
+        Console.WriteLine($"BASE {baseInt}");
+        Console.WriteLine($"First input: {input1}");
+        Console.WriteLine($"Second input: {input2}");
+        Console.WriteLine();
+
         Console.WriteLine($"Result: {result} in base {baseInt}");
-        Console.WriteLine($"Result: {preResult,10} in decimal");
+        Console.WriteLine($"Result: {preResult, 10} in decimal");
         Console.WriteLine("(Press Esc or E to exit)");
         Console.WriteLine("(Enter B to work with a different base)");
         return Console.ReadKey();
--- obj/Debug/net9.0/Number Tools.AssemblyInfo.cs
+++ obj/Debug/net9.0/Number Tools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Number Tools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eb175d76260204752c65afe55b24264b96f17a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a7eff00f94d3874e6c039772c6838902ed0e516")]
 [assembly: System.Reflection.AssemblyProductAttribute("Number Tools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Number Tools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
